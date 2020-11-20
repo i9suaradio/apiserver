570e282b075978d294ad7a1eaa1f60efb6755385
Guilherme Pinheiro <guilherme@i9suaradio.com.br>
2020-11-20 12:19:42 -0300

    Ajuste yum update

--- servidor.sh
+++ servidor.sh
@@ -38,6 +38,7 @@ echo -e "\e[1m\e[92mAtualização inicial do sistema ..."
 echo -e "\e[1m\e[92m----------------------------------"
 reset_cor
 sudo yum -y update && sudo yum -y upgrade
+yum --assumeno update
 yum autoremove
 yum -y install htop nano ufw wget nethogs
 
