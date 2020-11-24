19184be85ee9e463fa7e5c2fddcfcbe507edc57d
Guilherme Pinheiro <guilherme@i9suaradio.com.br>
2020-11-23 23:01:39 -0300

    Ajuste yum update

--- servidor.sh
+++ servidor.sh
@@ -38,8 +38,28 @@ echo -e "\e[1m\e[92mAtualização inicial do sistema ..."
 echo -e "\e[1m\e[92m----------------------------------"
 reset_cor
 sudo yum -y update && sudo yum -y upgrade
-yum autoremove
-yum -y install htop nano ufw wget nethogs
+yum check-update
+yum -y install htop nano ufw wget nethogs unar certbot unzip
+
+############################
+
+echo -e "\e[1m\e[92m--------------------------"
+echo -e "\e[1m\e[92mConfigurando Firewall..."
+echo -e "\e[1m\e[92m--------------------------"
+reset_cor
+
+if promptsn "Liberar as portas padrão no firewall?"; then
+  echo -e "\e[1m\e[92mDigite o range que deseja liberar no firewall (Ex.: 3000 ou 3000-5000)... \e[0m"
+  read range
+  sudo firewall-cmd --add-service=http
+  sudo firewall-cmd --add-service=https
+  sudo firewall-cmd --zone=public --add-port=$range/tcp --permanent
+  sudo firewall-cmd --runtime-to-permanent
+  sudo firewall-cmd --reload
+else
+  echo "Ok, próxima tarefa..."
+  reset_cor
+fi
 
 ############################
 
@@ -86,11 +106,19 @@ if promptsn "Deseja instalar o Docker? "; then
     sudo usermod -aG docker $(whoami)
   fi
 
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+
   if promptsn "Deseja instalar o gerenciador de containers Portainer?"; then
     docker volume create portainer_data
     docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
     docker ps
   fi
+
+  if promptsn "Criar volume com de nome Storage?"; then
+    docker volume create --driver local --opt type=none --opt device=/home/$novousuario/www --opt o=bind storage
+  fi
+
 fi
 
 ############################
@@ -135,6 +163,9 @@ if promptsn "Deseja instalar o NVM - Node Version Manager? "; then
       echo -e "\e[1m\e[92mInstalando PM2..."
       npm install -g pm2
 
+      pm2 startup systemd
+      pm2 save
+
       echo -e "\e[1m\e[92mNode Version"
       pm2 --version
       reset_cor
@@ -143,10 +174,17 @@ if promptsn "Deseja instalar o NVM - Node Version Manager? "; then
 
 fi
 
+#Execucoes finais
+sudo systemctl enable --now certbot-renew.timer
+
 echo -e "\e[1m\e[92m-------------------------------"
 echo -e "\e[1m\e[92mScript executado com sucesso..."
 echo -e "\e[1m\e[92m-------------------------------"
 reset_cor
+echo -e "Adicione manualmente a linha abaixo:"
+echo -e "\e[1m\e[92mrenew_hook = systemctl reload rabbitmq"
+reset_cor
+echo -e "No arquivo: /etc/letsencrypt/renewal/example.com.conf"
 
 #Reset da cor
 reset_cor
